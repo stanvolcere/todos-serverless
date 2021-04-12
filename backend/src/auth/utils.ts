6d044871aec29420de9f0a13d3df2d95189aaacb
stanvolcere <stanvolcere@hotmail.com>
2021-04-13 00:07:22 +0400

    FEAT - auth utils

--- backend/src/auth/utils.ts
+++ backend/src/auth/utils.ts
@@ -8,6 +8,18 @@ import { JwtPayload } from './JwtPayload'
  * @returns a user id from the JWT token
  */
 export function parseUserId(jwtToken: string): string {
-  const decodedJwt = decode(jwtToken) as JwtPayload
-  return decodedJwt.sub
+	const decodedJwt = decode(jwtToken) as JwtPayload
+	return decodedJwt.sub
+}
+
+export function getToken(authHeader: string): string {
+	if (!authHeader) throw new Error('No authentication header')
+
+	if (!authHeader.toLowerCase().startsWith('bearer '))
+		throw new Error('Invalid authentication header')
+
+	const split = authHeader.split(' ')
+	const token = split[1]
+
+	return token
 }
